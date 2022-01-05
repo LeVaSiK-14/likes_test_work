45c13b6050e542cdc98807d08fd3cb67d7fbb8c4
leva-14 <lev201611@gmail.com>
2022-01-05 19:55:58 +0600

    Add Likes and Diclikes Model

--- likes/admin.py
+++ likes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from likes.models import Likes, DisLikes
 
-# Register your models here.
+class LikesAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created_at']
+
+admin.site.register(Likes, LikesAdmin)
+
+class DisLikesAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created_at']
+
+admin.site.register(DisLikes, DisLikesAdmin)
--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from posts.models import Posts
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Likes(models.Model):
+
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='like')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name = "Лайки"
+        verbose_name_plural = "Лайк"
+
+    def __str__(self):
+        return f"{self.author.username} -- {self.post.title} -- {self.created_at}"
+
+
+class DisLikes(models.Model):
+
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='dislike')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name = "Дизлайки"
+        verbose_name_plural = "Дизлайк"
+
+    def __str__(self):
+        return f"{self.author.username} -- {self.post.title} -- {self.created_at}"
