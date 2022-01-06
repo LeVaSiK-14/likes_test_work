01b727469c34850e9ab27cae5ba0a69442fb141b
leva-14 <lev201611@gmail.com>
2022-01-06 10:43:41 +0600

    Add PostsSerializer

---
+++ posts/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
+from posts.models import Posts
+
+class PostsSerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source = 'author.username')
+
+    class Meta:
+        model = Posts
+        fields = ['id', 'author', 'title', 'description', 'created_at', 'likes', 'dislikes']
+        read_only_fields = ['created_at', 'likes', 'dislikes']
