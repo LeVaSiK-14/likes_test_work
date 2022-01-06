724a032ba4ed1de2e5e733b4e99fb77ec6f6ee63
leva-14 <lev201611@gmail.com>
2022-01-06 11:13:28 +0600

    Add UserCreateSerializer

---
+++ accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework import exceptions
+
+User = get_user_model()
+
+class UserCreateSerializer(serializers.Serializer):
+
+    username = serializers.CharField(max_length=127)
+    password = serializers.CharField(max_length=127)
+    password_repeat = serializers.CharField(max_length=127)
+
+
+    def create(self, validated_data):
+        
+        username = validated_data['username']
+        password = validated_data['password']
+        password_repeat = validated_data['password_repeat']
+
+        if len(password) <= 8:
+            raise exceptions.ValidationError({"Password": 'Password is too short'})
+        else:
+            if password == password_repeat:
+                user = User.objects.create(username=username,)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+                return user
+            else:
+                raise exceptions.ValidationError({"Password": 'Passwords do not match'})
