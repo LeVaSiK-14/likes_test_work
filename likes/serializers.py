dcb52662c5e96b8bf6d691d8be0afda2dd40fdfd
leva-14 <lev201611@gmail.com>
2022-01-06 10:54:29 +0600

    Add DisLikesSerializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from likes.models import Likes, DisLikes
 
+
 class LikesSerializer(serializers.ModelSerializer):
 
     post = serializers.ReadOnlyField(source='post.title')
@@ -9,5 +10,17 @@ class LikesSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = Likes
+        fields = ['id', 'post', 'author', 'created_at', ]
+        read_only_fields = ['created_at', ]
+
+
+class DisLikesSerializer(serializers.ModelSerializer):
+    
+    post = serializers.ReadOnlyField(source='post.title')
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+
+        model = DisLikes
         fields = ['id', 'post', 'author', 'created_at', ]
         read_only_fields = ['created_at', ]
